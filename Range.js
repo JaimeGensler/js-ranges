b01266a5db785fdcd38f06ed2c5831fc9b704cbb
JaimeGensler <jaimegensler0@gmail.com>
2020-02-12 13:02:47 -0800

    Add range direction and map with index

--- Range.js
+++ Range.js
@@ -1,7 +1,14 @@
 class Range {
     constructor(start, end) {
-        if ((typeof start) !== (typeof end)) {
-            throw new TypeError('Range start and end values must be same type!');
+        if (typeof start !== typeof end) {
+            throw new TypeError('Range values must be same type!');
+        } else if (
+            (typeof start !== 'string' && typeof start !== 'number') ||
+            (typeof start === 'number' && Math.floor(start) !== start)
+        ) {
+            throw new TypeError(
+                'Range values must only be integers or strings!'
+            );
         }
         this.start = start;
         this.end = end;
@@ -9,17 +16,22 @@ class Range {
 
     map(callback) {
         const arr = [];
-        const isString = (typeof this.start === 'string')
-        const mapStart = (isString) ? this.start.charCodeAt(0) : this.start;
-        const mapEnd = (isString) ? this.end.charCodeAt(0) : this.end;
+        const isString = typeof this.start === 'string';
+        const mapStart = isString ? this.start.charCodeAt(0) : this.start;
+        const mapEnd = isString ? this.end.charCodeAt(0) : this.end;
+        const dir = this.start < this.end ? 1 : -1;
 
-        for (let i = mapStart; i <= mapEnd; i++) {
-            const callReturn = isString
-                ? callback(String.fromCharCode(i))
-                : callback(i);
+        for (
+            let element = mapStart, index = 0;
+            element <= mapEnd;
+            element += dir, index++
+        ) {
+            const callReturn = callback(
+                isString ? String.fromCharCode(element) : element,
+                index
+            );
 
             if (callReturn !== undefined) arr.push(callReturn);
-            if (isString && i === 90) i = 96; //jump for non-alpha chars inbetween upper and lowercase set
         }
 
         return arr;
@@ -27,11 +39,9 @@ class Range {
 
     reduce(callback, initial = 0) {
         if (typeof this.start === 'string') {
-
-        }
-        else {
+        } else {
             let acc = initial;
-            for(let i = this.start; i<= this.end; i++) {
+            for (let i = this.start; i <= this.end; i++) {
                 acc = callback(acc, i);
             }
             return acc;
@@ -39,17 +49,23 @@ class Range {
     }
 }
 
-let myRange = new Range(1, 100);
+let myRange = new Range(1, 20);
 console.log(
-    myRange.map( x => {if (x%3 === 0) return x;} )
+    myRange.map((x, i) => {
+        return x + i;
+    })
 );
 
 myRange = new Range('A', 'z');
 console.log(
-    myRange.map( x => { return x;} )
+    myRange.map(x => {
+        return x;
+    })
 );
 
-myRange = new Range(1, 5);
+myRange = new Range('a', 'z');
 console.log(
-    myRange.reduce( (acc, i) => { return acc + i;})
+    myRange.reduce((acc, i) => {
+        return acc + i;
+    })
 );
